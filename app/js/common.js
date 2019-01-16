0c6527de4ef001759bcaa69e7cfee6a91baa05c4
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2019-01-16 15:35:07 +0300

    created promo section desctop version

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
-  
+  // promo slider
+  var swiper = new Swiper('.promo__slider', {
+  	pagination: {
+  		el: '.promo__slider-pagination',
+  		clickable: true,
+  		renderBullet: function (index, className) {
+  			return '<span class="' + className + '">' + (index + 1) + '</span>';
+  		},
+  	},
+  });
+
 });
