731500c8741d5a769903e76d96663f0d651517a8
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2019-01-17 16:29:52 +0300

    created footer

--- app/js/common.js
+++ app/js/common.js
@@ -13,20 +13,43 @@ $(document).ready(function () {
   });
 
 
-// quantity
-$('.quantity__minus').click(function () {
-	var $input = $(this).parent().find('input');
-	var count = parseInt($input.val()) - 1;
-	count = count < 1 ? 1 : count;
-	$input.val(count);
-	$input.change();
-	return false;
-});
-$('.quantity__plus').click(function () {
-	var $input = $(this).parent().find('input');
-	$input.val(parseInt($input.val()) + 1);
-	$input.change();
-	return false;
-});
+  //slider
+  var swiper = new Swiper('.index-page__sort-slider', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: !0,
+    navigation: {
+      nextEl: '.index-page__sort-slider-arrow--prev',
+      prevEl: '.index-page__sort-slider-arrow--next',
+    },
+    breakpoints: {
+      576: {
+        slidesPerView: 1
+      },
+      768: {
+        slidesPerView: 2
+      },
+      1200: {
+        slidesPerView: 3
+      }
+    }
+  });
+
+
+  // quantity
+  $('.quantity__minus').click(function () {
+   var $input = $(this).parent().find('input');
+   var count = parseInt($input.val()) - 1;
+   count = count < 1 ? 1 : count;
+   $input.val(count);
+   $input.change();
+   return false;
+ });
+  $('.quantity__plus').click(function () {
+   var $input = $(this).parent().find('input');
+   $input.val(parseInt($input.val()) + 1);
+   $input.change();
+   return false;
+ });
 
 });
