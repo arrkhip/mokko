e43abd86c7b6d231eec8d914581bb898fe08ee0e
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2019-01-17 12:58:59 +0300

    created card-product and quantity

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
   // promo slider
   var swiper = new Swiper('.promo__slider', {
   	speed: 700,
@@ -10,5 +11,22 @@ $(document).ready(function () {
   		},
   	},
   });
+
+
+// quantity
+$('.quantity__minus').click(function () {
+	var $input = $(this).parent().find('input');
+	var count = parseInt($input.val()) - 1;
+	count = count < 1 ? 1 : count;
+	$input.val(count);
+	$input.change();
+	return false;
+});
+$('.quantity__plus').click(function () {
+	var $input = $(this).parent().find('input');
+	$input.val(parseInt($input.val()) + 1);
+	$input.change();
+	return false;
+});
 
 });
