93e7551ba4d91f48246152b9ec7cbd000b7c4e30
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2019-01-18 10:39:02 +0300

    add edits in slider-base

--- app/js/common.js
+++ app/js/common.js
@@ -14,8 +14,33 @@ $(document).ready(function () {
 
 
   //slider
-  var swiper = new Swiper('.slider-base', {
-    slidesPerView: 4,
+  // var swiper = new Swiper('.slider-base', {
+  //   slidesPerView: 4,
+  //   spaceBetween: 30,
+  //   loop: !0,
+  //   navigation: {
+  //     nextEl: '.slider-base__arrow--next',
+  //     prevEl: '.slider-base__arrow--prev',
+  //   },
+  //   breakpoints: {
+  //     576: {
+  //       slidesPerView: 1
+  //     },
+  //     768: {
+  //       slidesPerView: 2
+  //     },
+  //     1200: {
+  //       slidesPerView: 3
+  //     }
+  //   }
+  // });
+
+  var mySwiper = undefined;
+function initSwiper() {
+    var screenWidth = $(window).width();
+    if(screenWidth > 768 && mySwiper == undefined) {            
+        mySwiper = new Swiper('.slider-base', {            
+             slidesPerView: 4,
     spaceBetween: 30,
     loop: !0,
     navigation: {
@@ -23,17 +48,29 @@ $(document).ready(function () {
       prevEl: '.slider-base__arrow--prev',
     },
     breakpoints: {
-      576: {
-        slidesPerView: 1
-      },
-      768: {
+      992: {
         slidesPerView: 2
       },
       1200: {
         slidesPerView: 3
       }
     }
-  });
+        });
+    } else if (screenWidth < 768 && mySwiper != undefined) {
+        mySwiper.destroy();
+        mySwiper = undefined;
+        jQuery('.swiper-wrapper').removeAttr('style');
+        jQuery('.swiper-slide').removeAttr('style');            
+    }        
+}
+
+//Swiper plugin initialization
+initSwiper();
+
+//Swiper plugin initialization on window resize
+$(window).on('resize', function(){
+    initSwiper();        
+});
 
 
   // quantity
